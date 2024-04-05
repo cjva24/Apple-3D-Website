ee48cf902941584ec09c8c33fa41dae1dd226962
Cjva24 <cjvanauken2424@gmail.com>
2024-04-05 17:58:38 -0500

    init

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { appleImg, bagImg, searchImg } from '../utils'
+
+const navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src={appleImg} alt='Apple' width={14} height={18} />
+        <div>
+          {['Phones', 'Macbooks', 'Tablets'].map((nav
+            )=> (
+              <div key={nav}>
+                {nav}
+              </div>
+            ))}
+        </div>
+        <div>
+          <img src={searchImg} alt="search" width={18}
+          height={18} />
+          <img src={bagImg} alt="bag" width={18}
+          height={18} />
+        </div>
+      </nav>
+    </header>
+  )
+}
+
+export default navbar
